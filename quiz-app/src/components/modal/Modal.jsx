d388849c82e04c0eeb02e2eedc67f36c8ef850c0
suleymankayir <suleymantalhakayir@gmail.com>
2024-05-22 11:13:01 +0300

    quiz app last commit

---
+++ quiz-app/src/components/modal/Modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Modal.css'
+
+const Modal = ({score}) => {
+  return (
+    <div className='modal'>
+        <div className='modal-title'>
+            Score: {score}
+        </div>
+        <div onClick={()=> window.location = "/"} className='modal-btn'>
+            Try Again
+        </div>
+    </div>
+  )
+}
+
+export default Modal
--- quiz-app/src/components/questionCard/QuestionCard.jsx
+++ quiz-app/src/components/questionCard/QuestionCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./QuestionCard.css";
 const QuestionCard = ({
   questionsData,
@@ -9,18 +9,51 @@ const QuestionCard = ({
   modal,
   setModal,
 }) => {
-  return <div className="questionCard">
-    <div>
-        {count+1}/10
-        -
-        {questionsData[count]?.question}
-    </div>
-    {
-        questionsData[count]?answers?.map((answer,i)=>(
-            <button>{answer}</button>
-        ))
+  const [timer, setTimer] = useState(30);
+
+  const approvedChoice = (e) => {
+    console.log(e.currentTarget.value);
+    const checkAnswer =
+      e.currentTarget.value == questionsData[count]?.correct_answer;
+    if (checkAnswer) {
+      setScore(score + 100);
     }
-  </div>;
+    setCount(count + 1);
+    if (count == 9) setModal(true);
+    setTimer(30)
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(timer> 0){
+        setTimer(timer - 1);
+      }
+      if(timer == 0 < 10 && count < 10){
+        setCount(count + 1)
+        setTimer(30)
+      }else if(count>=10){
+        setModal(true)
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [timer]);
+
+  return (
+    <div className="questionCard">
+      <div className="questionCard-timer">{timer}</div>
+      <div className="questionCard-title">
+        {count + 1}/10 -{questionsData[count]?.question}
+      </div>
+      {questionsData[count]?.answers?.map((answer, i) => (
+        <button onClick={approvedChoice} key={i} value={answer}>
+          {answer}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default QuestionCard;
